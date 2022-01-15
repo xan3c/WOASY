18e2baa0fbb922d26f561574256aede36e4b47b0
ahmedsiad <ahmedsiadsemail@gmail.com>
2022-01-15 14:36:47 -0700

    commit my first changes

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Boat Game</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>h1{color: pink;}</style>
