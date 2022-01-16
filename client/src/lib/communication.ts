98bfcd8612b992660b8b37009342bbfca7f25f17
Sergii Penner <sergmister1@gmail.com>
2022-01-16 12:02:21 -0700

    afdafadfadfadf

--- client/src/lib/communication.ts
+++ client/src/lib/communication.ts
@@ -1,6 +1,6 @@
 import type { SceneObject } from "$lib/scene";
 
-let serverURL = "http://localhost:8000";
+let serverURL = "http://localhost:8080";
 
 export const get_scenario = async (): Promise<SceneObject> => {
 	let url = new URL("/scenario", serverURL);
