2cb506a934df8f397a47bfca954ede4940feb641
ColossalDaddy <97775281+ColossalDaddy@users.noreply.github.com>
2022-01-16 12:47:32 -0700

    Update main.py

--- server/main.py
+++ server/main.py
@@ -32,7 +32,7 @@ async def finished_handler(request):
     response = request.json
     # print(response)
     for player_id in response["saved"]:
-        dataset[player_id] = [dataset[player_id], True]
+        dataset[player_id] = [dataset[player_id][0], True]
     return json({"success": True})
 
 
