da566a798b875f779741e78f04a126a2aee796d5
ahmedsiad <ahmedsiadsemail@gmail.com>
2022-01-15 23:46:37 -0700

    cleaned code

--- client/src/lib/game.ts
+++ client/src/lib/game.ts
@@ -1,4 +1,22 @@
 import * as PIXI from "pixi.js";
+import { Key } from "$lib/input";
+import png from "static/images/test.png";
+import { Player } from "$lib/player";
+import type { SceneObject, CharacterObject } from "$lib/scene";
+import { NPC } from "./npc";
+
+let char1: CharacterObject;
+let char2: CharacterObject;
+let sceneObject: SceneObject = {
+	characters: [],
+	lifeboat: {
+		x: 0,
+		y: 0,
+		maxCapacity: 0,
+	},
+	maxTime: 0,
+};
+sceneObject.characters.push(char1, char2);
 
 export class Game {
 	app: PIXI.Application;
@@ -11,8 +29,9 @@ export class Game {
 	async start() {
 		this.app = new PIXI.Application({
 			view: this.canvas,
-			width: 800,
-			height: 800,
+			width: 400,
+			height: 400,
+			backgroundColor: 0xdddddd,
 			resolution: window.devicePixelRatio,
 			autoDensity: true,
 			antialias: true,
@@ -20,21 +39,34 @@ export class Game {
 
 		let container = new PIXI.Container();
 		this.app.stage.addChild(container);
+		container.sortableChildren = true;
 
-		let graphics = new PIXI.Graphics();
-		graphics.beginFill(0xffffff);
-		graphics.drawCircle(0, 0, 30);
-		graphics.endFill();
+		let player = new Player();
+		container.addChild(player);
+		player.zIndex = 10;
 
-		this.app.stage.addChild(graphics);
+		let keyW = new Key("w");
+		let keyS = new Key("s");
+		let keyA = new Key("a");
+		let keyD = new Key("d");
+
+		let keySpace = new Key(" ");
+
+		let npcList = [];
+		for (let i = 0; i < 4; i++) {
+			let npc = new NPC(sceneObject.characters[i], this, player);
+			this.app.stage.addChild(npc);
+			npcList.push(npc);
+		}
 
 		let tick = 0;
 
 		const animate = (dt: number) => {
-			graphics.x = 400 + 100 * Math.cos(tick / 20);
-			graphics.y = 400 + 100 * Math.sin(tick / 20);
-
 			tick += 1;
+
+			player.move(keyW, keyS, keyA, keyD);
+
+			npcList.forEach((npc) => npc.tick());
 		};
 
 		this.app.ticker.add(animate);
