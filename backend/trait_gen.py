d7ddae25d3642bb28b197912a799b6d72585983f
ColossalDaddy <97775281+ColossalDaddy@users.noreply.github.com>
2022-01-15 19:36:41 -0700

    Add files via upload

--- backend/trait_gen.py
+++ backend/trait_gen.py
@@ -2,23 +2,40 @@ from sklearn.preprocessing import OneHotEncoder
 import random
 
 def trait_gen():
-  wealth = ['rich', 'poor']
-  criminal = ['innocent', 'murderer', 'robber']
-  mental_state = ['happy', 'depressed']
-  origin = ['canadian', 'american', 'immigrant', 'european']
-  parenthood = ['parent', 'non-parent']
+    wealth = ['rich', 'poor']
+    criminal = ['innocent', 'murderer', 'robber']
+    mental_state = ['happy', 'depressed']
+    origin = ['Canadian', 'American', 'immigrant', 'European']
+    parenthood = ['parent', 'non-parent']
 
-  enc = OneHotEncoder(categories=[wealth, criminal, mental_state, origin, parenthood])
+    enc = OneHotEncoder(categories=[wealth, criminal, mental_state, origin, parenthood])
 
-  x = [['rich', 'innocent', 'happy', 'canadian', 'parent']]
-  enc.fit(x)
-  
-  categories = [wealth, criminal, mental_state, origin, parenthood]
-  traits = []
-  for i in categories:
-    traits.append(random.choice(i))
+    x = [['rich', 'innocent', 'happy', 'canadian', 'non-parent']]
+    enc.fit(x)
 
-  result = enc.transform([traits]).toarray()  
+    categories = [wealth, criminal, mental_state, origin, parenthood]
+    traits = []
+    for i in categories:
+        traits.append(random.choice(i))
 
-  return result
-  
+    bio = "Currently, this person is {} and {}.".format(traits[0], traits[2]) 
+    if traits[1] == 'innocent':
+        bio += " They have a clean criminal record"
+    elif traits[1] == "robber":
+        bio += " They have a criminal record of bank robbery"
+    else:
+        bio += " They have a criminal record of commiting murder"
+
+    if traits[3] == 'immigrant':
+        bio += ', is an immigrant'
+    else:
+        bio += ", is {}".format(traits[3])
+
+    if traits[4] == 'parent':
+        bio += ", and has kids."
+    else:
+        bio += ", and has no kids."
+
+    result = enc.transform([traits]).toarray()  
+
+    return result, traits, bio
